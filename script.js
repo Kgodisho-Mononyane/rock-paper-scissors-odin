9b49c3aae33caaeeed22c3398187e9331f3d7c76
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2025-08-28 11:45:56 +0200

    add pseudocode of all the functions

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+/*
+FUNCTION getComputerChoice
+    GET random number
+    IF random number == 1 THEN
+        RETURN rock
+    ELSE IF random number == 2 THEN
+        RETURN paper
+    ELSE
+        RETURN scissors
+
+FUNCTION getHumanChoice
+    GET humanChoice of rock paper or scissors
+    RETURN humanChoice (make it case sensitive)
+
+FUNCTION playGame
+    INIT humanScore as 0
+    INIT computerScore as 0
+
+    FUNCTION playRound
+        const computerChoice as getComputerChoice
+        const humanChoice as getHumanChoice
+
+        IF player beats computer THEN
+            increment humanScore by 1
+            console.log(You win! X beats Y)
+        Else
+            increment ComputerScore by 1
+            console.log(You lose! X beats Y)
+
+    CALL playRound()
+    CALL playRound()
+    CALL playRound()
+    CALL playRound()
+    CALL playRound()
+
+Call playGame()
+*/
+
 let humanScore = 0;
 let computerScore = 0;
 
